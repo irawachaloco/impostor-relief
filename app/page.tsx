f14de1a89613513e03af3cfee18d17ad427576fa
Omar A Secas <omarasecas@omars-air.lan>
2024-09-19 10:45:16 -0600

    somo format and copy changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>Impostor Relief</h1>
-
-      <h2>
+    <div className="items-center justify-items-center min-h-screen_ p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <h1 className="font-bold text-xl pb-4">
         Why feel like a &quot;fraudulent professional&quot; when you can embrace
-        your inner imposter? Let&apos;s get impostorie. This will be a scratch
-        book for practicing while learning some real developer stuff.
+        your inner imposter? Let&apos;s get impostorie.
+      </h1>
+      <h2>
+        This will be a scratch book for practicing while learning some real
+        developer stuff.
       </h2>
     </div>
   );
