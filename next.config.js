73fbe16c4d3fce12346c9e509e5d6f2bfddc84fd
Omar A Secas <omarasecas@mac.lan>
2025-06-02 18:55:54 -0600

    changed next config file from mjs to js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  basePath: "/impostor-relief",
+  assetPrefix: "/impostor-relief",
+};
+
+module.exports = nextConfig;
