23615281cfcaa4a16f9b5938ff6722fd4668a848
Omar A Secas <omarasecas@Omars-MacBook-Air.local>
2025-06-02 21:12:38 -0600

    NavBar style fix

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -44,7 +44,7 @@ const NavItem = ({ link, text }: { link: string; text: string }) => {
     <Link
       href={link}
       prefetch={false}
-      className={`flex justify-center w-full md:w-auto border-b md:border-b-0 border-[#f2f2f2] py-2 md:py-0 text-shadow-[0_4px_28px_#7d7d7d]`}
+      className={`flex justify-center w-full md:w-auto border-b md:border-b-0 border-[#f2f2f2] py-2 md:py-0 md:text-shadow-[0_4px_28px_#7d7d7d]`}
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
       style={{
@@ -65,42 +65,44 @@ const NavBar = () => {
   };
 
   return (
-    <nav
-      className="px-8 sm:px-20  flex items-end min-h-[4em] min-w-full _w-full relative"
-      role="navigation"
-      aria-label="Main navigation"
-    >
-      <AnimatedBackground />
-      {/* <AnimatedBackground fill_color={"bg-[#e8e8e8]/90"} /> */}
-      <div className="flex items-end justify-between w-full">
-        <div className="flex h-[1.75em] md:h-[2.25em] flex-auto overflow-hidden">
-          <Link
-            href="/"
-            prefetch={false}
-            aria-label="Home"
-            className="hover:scale-[101%]"
+    <div className="overflow-hidden">
+      <nav
+        className="px-8 sm:px-20 flex items-end h-[4em] min-w-full relative"
+        role="navigation"
+        aria-label="Main navigation"
+      >
+        <AnimatedBackground />
+        {/* <AnimatedBackground fill_color={"bg-[#e8e8e8]/90"} /> */}
+        <div className="flex items-end justify-between w-full">
+          <div className="flex h-[1.75em] md:h-[2.25em] flex-auto">
+            <Link
+              href="/"
+              prefetch={false}
+              aria-label="Home"
+              className="hover:scale-[101%]"
+            >
+              <span className="sr-only">Impostor Relief Home</span>
+              <span className="text-gray-500/50 animated-text font-bold text-[2em] md:text-[2.5em] max-width">
+                Impostor Relief
+              </span>
+            </Link>
+          </div>
+          <div className="flex items-center gap-4 md:hidden">
+            {/* Burger button */}
+            <BurgerButton handleMenuToggle={handleMenuToggle} isOpen={isOpen} />
+          </div>
+          <div
+            id="main-menu"
+            className={`${
+              isOpen ? "flex" : "hidden"
+            } absolute top-[100%] md:top-auto md:relative md:flex flex-col md:flex-row right-0 items-center md:items-auto w-full md:w-auto bg-[#e8e8e8] md:bg-[#e8e8e8]/0 md:gap-4`}
           >
-            <span className="sr-only">Impostor Relief Home</span>
-            <span className="text-gray-500/50 animated-text font-bold text-[2em] md:text-[2.5em] max-width">
-              Impostor Relief
-            </span>
-          </Link>
+            <NavItem link="/learning" text="Learning" />
+            <NavItem link="/gallery" text="Gallery" />
+          </div>
         </div>
-        <div className="flex items-center gap-4 md:hidden">
-          {/* Burger button */}
-          <BurgerButton handleMenuToggle={handleMenuToggle} isOpen={isOpen} />
-        </div>
-        <div
-          id="main-menu"
-          className={`${
-            isOpen ? "flex" : "hidden"
-          } absolute top-[100%] md:top-auto md:relative md:flex flex-col md:flex-row right-0 items-center md:items-auto w-full md:w-auto bg-[#e8e8e8] md:bg-[#e8e8e8]/0 md:gap-4 p-4`}
-        >
-          <NavItem link="/learning" text="Learning" />
-          <NavItem link="/gallery" text="Gallery" />
-        </div>
-      </div>
-    </nav>
+      </nav>
+    </div>
   );
 };
 
